2b53a59527b0ad461d35cb15066c2fd1f80739e9
anton.concheiro <anton.concheiro@udc.es>
2021-05-13 17:16:56 +0200

    Acabado t-test

--- src/main/java/es/udc/fi/ri/mipractica/Compare.java
+++ src/main/java/es/udc/fi/ri/mipractica/Compare.java
@@ -1,11 +1,17 @@
 package es.udc.fi.ri.mipractica;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+import org.apache.commons.math3.stat.inference.TTest;
+import java.lang.Double;
+
 public class Compare {
 	
 	static String results1 = "results1";
 	static String results2 = "results2";
 	static String test = "t";
-	static float alpha = 0.0f;
+	static double alpha = 0.0;
 	
 	private static void parseArguments(String[] args) {
 
@@ -43,10 +49,78 @@ public class Compare {
             	}
             }
         }
-    }
+	}
+	
+	private static void doTTest(Scanner sc1, Scanner sc2) {
+		List<Double> lst1 = new ArrayList<Double>();
+		List<Double> lst2 = new ArrayList<Double>();
+		
+		while (sc1.hasNextLine()) {
+			String[] linesc1 = sc1.nextLine().split(" ");
+			double sample1 = Double.parseDouble(linesc1[linesc1.length-1]);
+			lst1.add(sample1);
+		}
+		while (sc2.hasNextLine()) {
+			String[] linesc2 = sc2.nextLine().split(" ");
+			double sample2 = Double.parseDouble(linesc2[linesc2.length-1]);
+			lst2.add(sample2);
+		}
+		double[] samples1=new double[lst1.size()];
+		double[] samples2=new double[lst2.size()];
+		int index = 0;
+		for(final Double value:lst1)
+			samples1[index++]=value;
+		index=0;
+		for(final Double value:lst2)
+			samples2[index++]=value;
+		TTest tTest = new TTest();
+		double pvalue = tTest.pairedTTest(samples1,samples2);
+		boolean result = tTest.pairedTTest(samples1,samples2, alpha);
+		String[] div = String.valueOf(pvalue).split("\\.");
+		System.out.println("Results for t-test");
+		System.out.println("Result\tP-value");
+		System.out.print("------\t");
+		for(int i = 0; i< div[1].length()+2;i++)
+			System.out.print("-");
+		System.out.println();
+		System.out.println(result+"\t"+pvalue);
+	}
+	
+	private static void doWTest(Scanner sc1, Scanner sc2) {
+		
+	}
 	
     public static void main( String[] args )
     {
     	parseArguments(args);
+    	
+    	Scanner reader1 = null;
+    	Scanner reader2 = null;
+    	
+    	try {
+    		File file1 = new File(results1);
+    		reader1 = new Scanner(file1);
+    	}catch(FileNotFoundException e) {
+    		System.err.println("File results1 does not exist");
+            System.exit(1);
+    	}
+    	
+    	try {
+    		File file2 = new File(results2);
+    		reader2 = new Scanner(file2);
+    	}catch(FileNotFoundException e) {
+    		System.err.println("File results2 does not exist");
+            System.exit(1);
+    	}
+    	
+    	switch(test) {
+    	case "t": doTTest(reader1, reader2);
+    			 break;
+    	case "wilcoxon": doWTest(reader1,reader2);
+    			 break;
+    	default: System.err.println("Error in test. Test not recognized");
+        		 System.exit(1);
+    	}
+    	
     }
 }
